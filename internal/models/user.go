64d9591c91c6ebfbb06ffb1307ced4d77c41f877
himanshu <himanshu24rai@gmail.com>
2026-01-26 11:42:07 +0530

    feat : upgrade user model with profile fields

--- internal/models/user.go
+++ internal/models/user.go
@@ -7,13 +7,19 @@ import(
 )
 
 type User struct{
-	ID            uuid.UUID  `gorm:"type:uuid;primaryKey"`
-	Name          string     `gorm:"not null"`
-	Email         string     `gorm:"unique;not null"`
-	PasswordHash  string     `gorm:"not null"`
-	Role          string     `gorm:"not null;default:'participant'"`
-	CreatedAt     time.Time
-	UpdatedAt     time.Time
+	ID            uuid.UUID  `gorm:"type:uuid;primaryKey" json:"id"`
+	Name          string     `gorm:"not null" json:"name"`
+	Email         string     `gorm:"unique;not null" json:"email"`
+	PasswordHash  string     `gorm:"not null" json:"-"`
+	Role          string     `gorm:"not null;default:'participant'" json:"role"`
+	
+	Department    string     `json:"department"`
+	Year          int        `json:"year"`
+	Phone         string     `json:"phone"`
+	IsVerfied	  bool       `gorm:"default:false" json:"is_verified"`
+	
+	CreatedAt     time.Time  `json:"created_at"`
+	UpdatedAt     time.Time  `json:"updated_at"`
 }
 
 func (u *User) BeforeCreate(tx *gorm.DB) error{
